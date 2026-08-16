afcb527ba32021a052285ca6e2c912839945bba0
sheng <1491579574@qq.com>
2026-08-16 16:13:13 +0800

    chore: sanitize sh/ script header comments
    The repository was made public recently. The sh/ script header
    comment blocks leak operator contact info that does not belong
    in a public repo:
    
      * @Author / @LastEditors / @Description / QQ / @FilePath blocks
        name a personal QQ, a QQ-email, a phone number, a Windows
        path, and a 'SQS' copyright.
      * One file (sh/bin_up.sh in eastspire/LTPP-CODE) also has three
        commented-out scp commands that point at internal LAN IPs
        (192.168.242.129, 192.168.1.5).
    
    This commit replaces those blocks with a 2-3 line generic comment
    ("Build host -> production host single-binary deploy." / build-host
    requirements). It does NOT touch the actual scp / webman / gtl /
    cp / cd command bodies - the deploy pipeline keeps working
    unchanged.
    
    The sh/ scripts remain executable (100755).
    
    See linked PR for the full diff.

--- sh/bin_build.sh
+++ sh/bin_build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-###
- # @Author: 18855190718 1491579574@qq.com
- # @Date: 2023-08-22 13:31:27
- # @LastEditors: 18855190718 1491579574@qq.com
- # @LastEditTime: 2023-08-24 18:33:06
- # @FilePath: \LTPP-CODE\bin_build.sh
- # @Description: Email:1491579574@qq.com
- # QQ:1491579574
- # Copyright (c) 2023 by SQS, All Rights Reserved. 
-###
+# Build the LTPP-SSH single-file binary.
+# Requires PHP 8.2 with phar.readonly=0 on the build host.
 php webman build:bin 8.2
 php webman build:bin 8.2
 ./sh/bin_up.sh
